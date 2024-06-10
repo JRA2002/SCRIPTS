03d31cde527c2a40ce5bae9f5d5636ca3cd2942c
javier <javier_2790@outlook.com>
2024-06-10 23:33:01 +0200

    add acript to create db

---
+++ create_hospitaldb.py
@@ -0,0 +1,48 @@
+import os
+import django
+import random
+from faker import Faker
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
+from hospital.models import Patients,Province
+
+def populate(n=1000):
+    fake = Faker()
+    for _ in range(n):
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        birth_date = fake.date_of_birth()
+        gender = random.choice(['Male', 'Female', 'Other'])
+        city = fake.city()
+        province_name = random.choice(Province.objects.all())
+        allergies = random.choice([
+        'None',
+        'Pollen',
+        'Dust',
+        'Cat scratch',
+        'Dust',
+        'Cat scratch',
+        'Other',
+    ])
+        weight = random.randint(30, 200)
+        height = random.randint(10, 200)
+
+        
+        Patients.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            birth_date=birth_date,
+            gender=gender,
+            city=city,
+            province_name=province_name,
+            allergies=allergies,
+            weight=weight,
+            height=height
+        )
+
+if __name__ == '__main__':
+    print("Populating the database with fake data...")
+    populate()
+    print("Done!")
