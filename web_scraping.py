c24cff87740e46c345431844d3c7b6fc6329a58a
javier <javier_2790@outlook.com>
2024-06-13 19:59:54 +0200

    finalize scrappy

--- web_scraping.py
+++ web_scraping.py
@@ -1,10 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-url = "https://www.game.es/OFERTAS/PACK/PACKS/PLAYSTATION-5-SLIM-STELLAR-BLADE/P05166"
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
+item_search = input("What product do you want to search for :")
 
-prices = doc.find_all(string="€")
-#parent = prices[1].parent
-print(prices)
+url = f"https://www.newegg.ca/p/pl?d={item_search}"
+page = requests.get(url).text
+doc  = BeautifulSoup(page, "html.parser")
+
+page_text = doc.find(class_="list-tool-pagination-text").strong.text
+pages = int(page_text.split('/')[1])
+items_found = {}
+
+for page in range(1, pages + 1):
+    url = f"https://www.newegg.ca/p/pl?d={item_search}&page={page}"
+    page = requests.get(url).text
+    doc  = BeautifulSoup(page, "html.parser")
+
+    div = doc.find(class_="item-cells-wrap border-cells short-video-box items-grid-view four-cells expulsion-one-cell")
+    items = div.find_all(string=re.compile(item_search))
+
+    for item in items:
+        parent = item.parent
+        if parent.name != "a":
+            continue
+
+        link = parent["href"]
+        next_parent = item.find_parent(class_="item-container")
+        price = next_parent.find(class_="price-current").strong.text.replace(',','')
+
+        items_found[item] = {"price":int(price), "link":link}
+
+sorted_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
+
+for item in sorted_items:
+    print(item[0])
+    print(item[1]['price'])
+    print(item[1]['link'])
+    print('--------------------------------------------')
