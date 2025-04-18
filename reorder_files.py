cfee57c3b634f306b9b90fcb57e620d8c5389328
javier <javier_2790@outlook.com>
2025-04-18 22:45:01 +0200

    feat: update reorder script

--- reorder_files.py
+++ reorder_files.py
@@ -5,29 +5,41 @@ from pathlib import Path
 source_dir = "/Users/javier/Downloads/"
 dest_dir = "/Users/javier/Downloads"
 
+def move_files(source_dir, dest):
+        try:
+            shutil.move(source_dir, dest)
+        except FileNotFoundError:
+            print(f"File not found: {source_dir}")
+        except PermissionError:
+            print(f"Permission denied: {source_dir}")
+        except Exception as e:
+            print(f"Error moving file {source_dir}: {e}")
+        except FileExistsError:
+            print(f"File already exists: {source_dir}")
+
 def move_data(source,dest):
     
     for filename in os.listdir(source):
 
         name, extension = os.path.splitext(filename)
         source_dir = os.path.join(source + filename)
-        
-        if extension == '.pdf':
-            dest = "/Users/javier/Downloads/PDF"
-            print(f'archivo pdf finded : {filename}')
-            shutil.move(source_dir, dest)
 
+        if extension == '':
+            continue
+        elif extension == '.pdf':
+            dest = "/Users/javier/Downloads/PDF"
         elif extension in ['.jpeg', '.jpg', '.png', '.webp']:
             dest = "/Users/javier/Downloads/IMAGES"
-            shutil.move(source_dir, dest)
-
-        elif extension in ['.docx', '.doc']:
-            dest = "/Users/javier/Downloads//WORD"
-            shutil.move(source_dir, dest)
+        elif extension in ['.docx', '.doc', '.txt']:
+            dest = "/Users/javier/Downloads/WORD"
         elif extension in ['.xlsx', '.xls']:
-            dest = "/Users/javier/Downloads//EXCEL"
-            shutil.move(source_dir, dest)
+            dest = "/Users/javier/Downloads/EXCEL"
         else:
-            print('not type ')
+            dest = "/Users/javier/Downloads/OTHER"
 
-move_data(source_dir, dest_dir)
+        move_files(source_dir, dest)
+        
+
+        
+if __name__ == "__main__":
+    move_data(source_dir, dest_dir)
